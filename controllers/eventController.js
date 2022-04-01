075ddc4164e48e6dbe16c9b036652504c2665ad1
Beau Seate <rseate@uncc.edu>
2022-04-01 00:25:40 -0400

    Fixed CSS and edit errors from proj 2

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -11,33 +11,58 @@ exports.newConnection = (req, res)=>{
 };
 
 exports.createConnection = (req, res)=>{
-    let event = req.body;
-    model.save(event);
-    res.redirect('/events');
+     //res.send('Created a new story');
+     let event = new model(req.body);//Create new story document in mongodb
+     event.save()//Insert in db
+     .then(event=> res.redirect('/events'))
+     .catch(err=>{
+         if(err.name === 'ValidationError'){
+             err.status = 400;
+         }
+         next(err);
+     });
 };
 
 exports.showConnection = (req, res, next)=>{
     let id = req.params.id;
-    let event = model.findById(id);
-    if(event){
-        res.render('./event/connection', {event});
-    }else{
-        let err = new Error('Cannot find a event with id ' + id);
-        err.status = 404;
-        next(err);
+    //obj id is 24 bits hexadecmal string
+    if(!id.match(/^[0-9a-fA-F]{24}$/)) {
+            let err = new Error('Invalid event ID');
+            err.status = 400;
+            return next(err);
     }
+    model.findById(id)
+    .then(event=>{
+        if(event) {
+            return res.render('./event/connection', {event});
+        } else {
+            let err = new Error('Cannot find a event with id ' + id);
+            err.status = 404;
+            next(err);
+        }
+    })
+    .catch(err=>next(err))
 };
 
 exports.editConnection = (req, res, next)=>{
     let id = req.params.id;
-    let event = model.findById(id);
-    if(event){
-        res.render('./event/edit', {event});
-    }else{
-        let err = new Error('Cannot find a event with id ' + id);
-        err.status = 404;
-        next(err);
+    //obj id is 24 bits hexadecmal string
+    if(!id.match(/^[0-9a-fA-F]{24}$/)) {
+            let err = new Error('Invalid event ID');
+            err.status = 400;
+            return next(err);
     }
+    model.findById(id)
+    .then(event=>{
+        if(event) {
+            return res.render('./event/edit', {event});
+        } else {
+            let err = new Error('Cannot find a event with id ' + id);
+            err.status = 404;
+            next(err);
+        }
+    })
+    .catch(err=>next(err))
 };
 
 exports.updateConnection = (req, res, next)=>{
